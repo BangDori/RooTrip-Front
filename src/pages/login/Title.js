6bbfca81795b5813007e9870a4e746eb8b69022a
BangDori <qudwns8865@naver.com>
2023-03-05 18:36:02 +0900

    [Feat] 로그인 화면 Title

---
+++ src/pages/login/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className='Login_logo'>
+      <h3>Root Trip</h3>
+      <span className='Logotext'>
+        <span className='text1'>여행의 재미를 더하는 </span>
+        <span className='text2'>SNS</span>
+      </span>
+    </div>
+  );
+};
+
+export default Title;
