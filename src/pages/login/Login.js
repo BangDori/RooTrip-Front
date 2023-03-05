62d2876cf5d8b348db75498d0ed4c7fc971aa3a0
BangDori <qudwns8865@naver.com>
2023-03-05 21:16:42 +0900

    [Feat] 로그인 상태 관리 및 로그인 통신

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,27 +1,68 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { login } from '../../services/user';
 
 const LocalLogin = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+  const { email, password } = form;
+
+  const onChange = useCallback(
+    (e) => {
+      setForm({
+        ...form,
+        [e.target.name]: e.target.value,
+      });
+    },
+    [form],
+  );
+
+  const onLogin = useCallback(
+    async (e) => {
+      e.preventDefault();
+
+      try {
+        // accessToken, refreshToken 저장
+        const { accessToken, refreshToken } = await login(form);
+
+        setForm({
+          email: '',
+          password: '',
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    [form],
+  );
+
   return (
-    <div className='login_index'>
+    <form className='login_index' action='' method='post' onSubmit={onLogin}>
       <input
         className='logintext'
-        type='text'
-        name='ID'
+        name='email'
+        value={email}
         placeholder='사용자 이메일 또는 아이디'
+        onChange={onChange}
       />
       <input
         className='logintext'
-        type='text'
-        name='PW'
+        name='password'
+        value={password}
         placeholder='비밀번호'
+        onChange={onChange}
       />
-      <button type='button' className='loginbtn'>
+      <button type='submit' className='loginbtn'>
         로그인
       </button>
-      <button type='button' className='gosignupbtn'>
-        회원 가입
-      </button>
-    </div>
+      <Link to='/register'>
+        <button type='button' className='gosignupbtn'>
+          회원 가입
+        </button>
+      </Link>
+    </form>
   );
 };
 
