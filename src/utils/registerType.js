e3d9405eb9a68a34f9b3bf4fe15f6f503a63174b
BangDori <qudwns8865@naver.com>
2023-03-08 00:13:28 +0900

    [Feat] Register Type Constant

---
+++ src/utils/registerType.js
@@ -0,0 +1,9 @@
+// 회원가입 입력 필드 체크를 위한 타입 설정
+
+const NAME = 'name';
+const EMAIL = 'email';
+const NICKNAME = 'nickname';
+const PASSWORD = 'password';
+const CPASSWORD = 'cpassword';
+
+export { NAME, EMAIL, NICKNAME, PASSWORD, CPASSWORD };
