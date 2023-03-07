1659c099815c760e566e1f0fff86c71e547fb4e1
BangDori <qudwns8865@naver.com>
2023-03-08 00:27:09 +0900

    [Refacotr] 회원가입 페이지 코드 수정

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,22 +1,17 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import '../../styles/register/register.scss';
 
-const Register = ({ form, onInput, onRegister }) => {
-  const { name, email, nickname, password } = form;
-  const [gender, setGender] = useState('남자');
-  const [repassword, setRepassword] = useState('');
+const Register = ({ form, messages, onInput, onCheck, onRegister }) => {
+  const { name, email, nickname, password, gender, cpassword } = form;
+
   const refName = useRef(null);
-  const errorText = useRef(null);
 
   useEffect(() => {
     refName.current.focus();
   }, []);
 
   return (
-    <form
-      className='Register_main'
-      onSubmit={(e) => onRegister(e, repassword, errorText)}
-    >
+    <form className='Register_main' onSubmit={onRegister}>
       <div className='User_data'>
         <div className='User_data_name'>
           <span>성명</span>
@@ -24,7 +19,6 @@ const Register = ({ form, onInput, onRegister }) => {
           <span>닉네임</span>
           <span>비밀번호</span>
           <span>비밀번호 확인</span>
-          <span>전화번호</span>
         </div>
         <div className='User_data_content'>
           <div className='Name'>
@@ -34,20 +28,23 @@ const Register = ({ form, onInput, onRegister }) => {
               name='name'
               value={name}
               onChange={onInput}
+              onBlur={(e) => onCheck(e.target.name, e.target.value)}
               placeholder='이름을 입력해주세요'
             />
             <div className='check_box'>
               <span className='check_sex'>
                 <button
                   type='button'
+                  name='gender'
+                  value='M'
                   style={{
-                    background: `${gender === '남자' ? '#0c4fc7' : 'white'}`,
-                    color: `${gender === '남자' ? 'white' : 'black'}`,
-                    fontWeight: `${gender === '남자' ? 'bold' : ''}`,
-                    width: `${gender === '남자' ? '50px': '45px'}`,
-                    height: `${gender === '남자' ? '50px': '45px'}`
+                    background: `${gender === 'M' ? '#0c4fc7' : 'white'}`,
+                    color: `${gender === 'M' ? 'white' : 'black'}`,
+                    fontWeight: `${gender === 'M' ? 'bold' : ''}`,
+                    width: `${gender === 'M' ? '50px' : '45px'}`,
+                    height: `${gender === 'M' ? '50px' : '45px'}`,
                   }}
-                  onClick={() => setGender('남자')}
+                  onClick={onInput}
                 >
                   남
                 </button>
@@ -55,18 +52,21 @@ const Register = ({ form, onInput, onRegister }) => {
               <span className='check_sex'>
                 <button
                   type='button'
+                  name='gender'
+                  value='W'
                   style={{
-                    background: `${gender === '여자' ? '#0c4fc7' : 'white'}`,
-                    color: `${gender === '여자' ? 'white' : 'black'}`,
-                    fontWeight: `${gender === '여자' ? 'bold' : ''}`,
-                    width: `${gender === '여자' ? '50px': '45px'}`,
-                    height: `${gender === '여자' ? '50px': '45px'}`
+                    background: `${gender === 'W' ? '#0c4fc7' : 'white'}`,
+                    color: `${gender === 'W' ? 'white' : 'black'}`,
+                    fontWeight: `${gender === 'W' ? 'bold' : ''}`,
+                    width: `${gender === 'W' ? '50px' : '45px'}`,
+                    height: `${gender === 'W' ? '50px' : '45px'}`,
                   }}
-                  onClick={() => setGender('여자')}
+                  onClick={onInput}
                 >
                   여
                 </button>
               </span>
+              {messages.name && <span>{messages.name}</span>}
             </div>
           </div>
           <div>
@@ -75,9 +75,10 @@ const Register = ({ form, onInput, onRegister }) => {
               name='email'
               value={email}
               onChange={onInput}
+              onBlur={(e) => onCheck(e.target.name, e.target.value)}
               placeholder='email을 입력해주세요'
             />
-            <span>※ 이메일 형식에 맞춰주세요</span>
+            {messages.email && <span>{messages.email}</span>}
           </div>
           <div>
             <input
@@ -85,52 +86,41 @@ const Register = ({ form, onInput, onRegister }) => {
               name='nickname'
               value={nickname}
               onChange={onInput}
+              onBlur={(e) => onCheck(e.target.name, e.target.value)}
               placeholder='닉네임을 입력해주세요'
             />
-            {/* 정규식으로 입력한 닉네임이 형식에 맞으면 파란색으로 * 사용가능한 닉네임입니다로 바뀌고 형식에 맞지 않다면 빨간색으로 * 사용할 수 없는 닉네임입니다로 바뀜 */}
-            <span>※ 한글, 영어, 숫자를 조합한 닉네임을 입력해주세요</span>
+            {messages.nickname && <span>{messages.nickname}</span>}
           </div>
           <div>
             <input
               type='text'
+              className='password'
               name='password'
               value={password}
               onChange={onInput}
+              onBlur={(e) => onCheck(e.target.name, e.target.value)}
               placeholder='비밀번호를 입력해주세요'
             />
-            {/* 정규식으로 입력한 닉네임이 형식에 맞으면 파란색으로 * 시용가능한 비밀번호입니다로 바뀜 */}
-            <span className='PWD_df'>
-              ※ 숫자, 영어, 특수문자를 포함해야 합니다!
-            </span>
+            {messages.password && <span>{messages.password}</span>}
           </div>
           <div>
             <input
               type='text'
-              name='password_check'
-              value={repassword}
-              onChange={(e) => setRepassword(e.target.value)}
+              className='cpassword'
+              name='cpassword'
+              value={cpassword}
+              onChange={onInput}
+              onBlur={(e) => onCheck(e.target.name, e.target.value)}
               placeholder='비밀번호를 한번 더 입력해주세요'
             />
-            {/* 위의 비밀번호랑 일치하면 파란색으로 * 비밀번호가 일치합니다로 바뀜 */}
-            <span>※ 위 입력한 비밀번호를 다시 입력해주세요</span>
-          </div>
-          <div>
-            <input
-              type='text'
-              name='phone'
-              value=''
-              //onChange={(e) => setRepassword(e.target.value)}
-              placeholder='전화번호를 입력해주세요'
-            />
-            {/* 위의 비밀번호랑 일치하면 파란색으로 * 비밀번호가 일치합니다로 바뀜 */}
-            <button className='certification_PN'>인증하기</button>
+            {messages.cpassword && <span>{messages.cpassword}</span>}
           </div>
         </div>
       </div>
       <div className='check_assign'>
         {/* 필수 누르면 가입하기 버튼 활성화 원래는 활성화 x -> 이거 내가 만들거 */}
         <label>
-          <input type='checkbox' name='' value=''></input>
+          <input type='checkbox' name='checkbox' />
           <span>통합 서비스 이용약관 (동의)</span>
         </label>
         <label>
@@ -140,7 +130,6 @@ const Register = ({ form, onInput, onRegister }) => {
         <div className='signbtn'>
           <button type='submit'>가입하기</button>
         </div>
-        <p ref={errorText} style={{ color: 'red' }}></p>
       </div>
     </form>
   );
