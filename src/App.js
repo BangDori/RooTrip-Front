a505b8922099a27bf81d1132e035ae5bfb9b75c8
BangDori <qudwns8865@naver.com>
2023-03-08 17:22:05 +0900

    [Rename] Index.js -> App.js 이동

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import Index from './pages/Index';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/login/Index';
+import Home from './pages/home/Index';
+import Register from './pages/register/Index';
+import useToken from './hooks/useToken';
 
 const App = () => {
-  return <Index />;
+  const accessToken = useToken();
+
+  return (
+    <Routes>
+      {accessToken ? (
+        <Route path='/' element={<Home />} />
+      ) : (
+        <Route path='/' element={<Login />} />
+      )}
+      <Route path='/' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+    </Routes>
+  );
 };
 
 export default App;
