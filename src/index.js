7a9cdde05c5b567916df48cc9a7be5226b777927
BangDori <qudwns8865@naver.com>
2023-03-08 17:22:33 +0900

    [Remove] Redux 관련 상태 제거

--- src/index.js
+++ src/index.js
@@ -4,20 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { legacy_createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './store';
 
-const store = legacy_createStore(rootReducer, composeWithDevTools());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 );
