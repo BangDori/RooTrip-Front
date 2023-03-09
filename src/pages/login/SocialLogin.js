ea139514fa6c63f5e70f7e9983b9fc2a382f407e
BangDori <qudwns8865@naver.com>
2023-03-10 01:27:12 +0900

    [Fix] window.location.href -> Link

--- src/pages/login/SocialLogin.js
+++ src/pages/login/SocialLogin.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SocialLogin = () => {
   return (
     <div className='social_login'>
-      <button type='button' className='kakaobtn'>
-        카카오톡 로그인
-      </button>
-      <button type='button' className='naverbtn'>
-        네이버 로그인
-      </button>
-      <button type='button' className='googlebtn'>
-        구글 로그인
-      </button>
+      <Link to={process.env.REACT_APP_KAKAO_AUTH_URL} state={{ type: 'kakao' }}>
+        <button className='kakaobtn'>카카오톡 로그인</button>
+      </Link>
+      <Link to={process.env.REACT_APP_NAVER_AUTH_URL} state={{ type: 'naver' }}>
+        <button className='naverbtn'>네이버 로그인</button>
+      </Link>
+      <Link
+        to={process.env.REACT_APP_GOOGLE_AUTH_URL}
+        state={{ type: 'google' }}
+      >
+        <button className='googlebtn'>구글 로그인</button>
+      </Link>
     </div>
   );
 };
