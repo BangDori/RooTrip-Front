9e66fcdce0a4b4b0094c042188616de13c5973be
BangDori <qudwns8865@naver.com>
2023-03-09 23:53:20 +0900

    [Feat] 단순 로그아웃 기능 추가

--- src/pages/home/Index.js
+++ src/pages/home/Index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { removeAccessToken, removeRefreshToken } from '../../utils/auth';
 
 const Index = () => {
+  const onRemove = () => {
+    removeAccessToken();
+    removeRefreshToken();
+    window.location.reload();
+  };
+
   return (
     <div>
       <h3>Home</h3>
+      <button onClick={onRemove}>토큰 만료</button>
     </div>
   );
 };
