d5daaf4c6115fce607903bcd85ffc302e090c606
BangDori <qudwns8865@naver.com>
2023-03-12 17:31:21 +0900

    [Style] 코드 프리터 적용

--- src/pages/login/Title.js
+++ src/pages/login/Title.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import Logo from '../../assets/Logo.png'
+import Logo from '../../assets/Logo.png';
 
 const Title = () => {
   return (
     <>
-    <div className=''>
-      <img className='Login_logo' src={Logo} alt="로고사진" />
-    </div> 
-    <span className='Logotext'>
-      <span className='text1'>여행의 재미를 더하는 </span>
-      <span className='text2'>SNS</span>
-    </span>
-    
+      <div className=''>
+        <img className='Login_logo' src={Logo} alt='로고사진' />
+      </div>
+      <span className='Logotext'>
+        <span className='text1'>여행의 재미를 더하는 </span>
+        <span className='text2'>SNS</span>
+      </span>
     </>
   );
 };
--- src/pages/register/Title.js
+++ src/pages/register/Title.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import '../../styles/register/register.scss'
-import Logo from '../../assets/Logo.png'
+import '../../styles/register/register.scss';
+import Logo from '../../assets/Logo.png';
 import { Link } from 'react-router-dom';
+
 const Title = () => {
-    return (
-        <Link to = '/..' className='Go_before_page'>
-        <div className='Register_Title'>
-            <img className='logo' src={Logo} alt="로고사진" />
-            <span className='content'>JOIN MEMBERS</span>
-        </div>
-        </Link>
-    );
+  return (
+    <Link to='/' className='Go_before_page'>
+      <div className='Register_Title'>
+        <img className='logo' src={Logo} alt='로고사진' />
+        <span className='content'>JOIN MEMBERS</span>
+      </div>
+    </Link>
+  );
 };
 
 export default Title;
