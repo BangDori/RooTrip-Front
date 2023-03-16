ca8fe4eb564101fef0d4df35f374dbb9133d5ffc
BangDori <qudwns8865@naver.com>
2023-03-16 21:44:04 +0900

    [Edit] accessToken

--- src/App.js
+++ src/App.js
@@ -8,20 +8,18 @@ const Login = loadable(() => import('@pages/login/Index'));
 const Register = loadable(() => import('@pages/register/Index'));
 const Auth = loadable(() => import('@components/Auth'));
 const NotFound = loadable(() => import('@components/NotFound'));
-const Email = loadable(() => import('@pages/register/certification/Email'));
 
 const App = () => {
   const accessToken = useToken();
 
   return (
     <Routes>
-      {!accessToken ? (
+      {accessToken ? (
         <Route path='/' element={<Home />} />
       ) : (
         <Route path='/' element={<Login />} />
       )}
       <Route path='/register' element={<Register />} />
-      <Route path='/register/certification' element={<Email />} />
       <Route path='/oauth/:provider/*' element={<Auth />} />
       <Route path='/*' element={<NotFound />} />
     </Routes>
