a7498ab819486e31caab97e2f172b1f32373d5d9
BangDori <qudwns8865@naver.com>
2023-03-20 01:14:27 +0900

    [Feat] 토큰의 만료 시간 추가

--- src/store/accessToken.js
+++ src/store/accessToken.js
@@ -6,12 +6,15 @@ const REMOVE = 'TOKEN/REMOVE';
 export const issue = createAction(ISSUE);
 export const remove = createAction(REMOVE);
 
-const accessTokenState = '';
+const accessTokenState = {
+  accessToken: '',
+  expireTime: 15 * 60 * 1000,
+};
 
 const accessToken = handleActions(
   {
-    [ISSUE]: (state, action) => action.payload,
-    [REMOVE]: (state, action) => '',
+    [ISSUE]: (state, action) => ({ ...state, accessToken: action.payload }),
+    [REMOVE]: (state, action) => ({ ...state, accessToken: '' }),
   },
   accessTokenState,
 );
