e5371729f126d42e37932b4902a444a5a28bb897
BangDori <qudwns8865@naver.com>
2023-03-21 01:33:10 +0900

    [Add] 미인증 에러 추가

--- src/constants/error.js
+++ src/constants/error.js
@@ -1,14 +1,18 @@
-// 유효성 에러
+// 회원가입 - 유효성 에러
 export const INVALID_NAME_ERROR = '유효하지 않은 이름입니다.';
 export const INVALID_EMAIL_ERROR = '유효하지 않은 이메일입니다.';
 export const INVALID_NICKNAME_ERROR = '유효하지 않은 닉네임입니다.';
 export const INVALID_PASSWORD_ERROR = '유효하지 않은 비밀번호입니다.';
 
-// 중복 에러
+// 회원가입 - 중복 에러
 export const DUPLICATED_EMAIL_ERROR = '이미 존재하는 이메일입니다.';
 export const DUPLICATED_NICKNAME_ERROR = '이미 존재하는 닉네임입니다.';
 
-// 비밀번호 에러
+// 회원가입 - 이메일 미인증 에러
+export const EMAIL_VERIFICATION_NOT_COMPLETED_ERROR =
+  '이메일 인증이 완료되지 않았습니다';
+
+// 회원가입 및 로그인 - 비밀번호 에러
 export const PASSWORD_MISMATCH_ERROR = '비밀번호가 일치하지 않습니다.';
 
 // 통신 에러
