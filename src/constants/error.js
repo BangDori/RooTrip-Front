94cf41a7f40bf1b4ad170d850a4b47766bc0a8e4
BangDori <qudwns8865@naver.com>
2023-03-20 18:28:05 +0900

    [Add] 에러 정보 추가

--- src/constants/error.js
+++ src/constants/error.js
@@ -1,3 +1,13 @@
+// 중복 에러
+export const DUPLICATED_EMAIL_ERROR = '이미 존재하는 이메일입니다.';
+export const DUPLICATED_NICKNAME_ERROR = '이미 존재하는 닉네임입니다.';
+
+// 유효성 에러
+export const INVALID_EMAIL_ERROR = '유효하지 않은 이메일입니다.';
+export const INVALID_NICKNAME_ERROR = '유효하지 않은 닉네임입니다.';
+export const INVALID_PASSWORD_ERROR = '유효하지 않은 비밀번호입니다.';
+
+// 통신 에러
 export const USER_LOGIN_FAILED_ERROR = 'USER_LOGIN_FAILED_ERROR ';
 export const USER_LOGOUT_FAILED_ERROR = 'USER_LOGOUT_FAILED_ERROR';
 export const TOKEN_REISSUE_ERROR = 'TOKEN_REISSUE_ERROR';
