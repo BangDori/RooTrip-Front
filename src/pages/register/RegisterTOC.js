6196ff1a9748cea046e70aca373391693812ef33
BangDori <qudwns8865@naver.com>
2023-03-21 01:16:36 +0900

    [Edit] 네이밍 수정 (성명 -> 이름, email -> 이메일)

--- src/pages/register/RegisterTOC.js
+++ src/pages/register/RegisterTOC.js
@@ -3,8 +3,8 @@ import React from 'react';
 const RegisterTOC = () => {
   return (
     <div className='User_data_name'>
-      <span>성명</span>
-      <span className='email_check'>email</span>
+      <span>이름</span>
+      <span className='email_check'>이메일</span>
       <span>닉네임</span>
       <span>비밀번호</span>
       <span>비밀번호 확인</span>
