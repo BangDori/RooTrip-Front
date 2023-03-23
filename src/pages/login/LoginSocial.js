08d6ccf4236df85ca51dc572707f3a8acfec8e00
BangDori <qudwns8865@naver.com>
2023-03-23 15:04:44 +0900

    [Design] 소셜 로그인 버튼 이미지로 대체

--- src/pages/login/LoginSocial.js
+++ src/pages/login/LoginSocial.js
@@ -2,6 +2,9 @@ import React from 'react';
 import loadable from '@loadable/component';
 import { Link } from 'react-router-dom';
 import { GOOGLE_AUTH_URL, KAKAO_AUTH_URL, NAVER_AUTH_URL } from '@config/auth';
+import naver from '@assets/naver.png';
+import kakao from '@assets/kakao.png';
+import google from '@assets/google.png';
 
 const SocialAuth = loadable(() => import('@components/SocialAuth'));
 
@@ -15,21 +18,27 @@ const LoginSocial = () => {
         state={{ type: 'kakao' }}
         onMouseEnter={onAuthLoad}
       >
-        <button className='kakaobtn'>카카오톡 로그인</button>
+        <button className='kakaobtn'>
+          <img className='Login_logo' src={kakao} alt='카카오 로그인 버튼' />
+        </button>
       </Link>
       <Link
         to={NAVER_AUTH_URL}
         state={{ type: 'naver' }}
         onMouseEnter={onAuthLoad}
       >
-        <button className='naverbtn'>네이버 로그인</button>
+        <button className='naverbtn'>
+          <img className='Login_logo' src={naver} alt='네이버 로그인 버튼' />
+        </button>
       </Link>
       <Link
         to={GOOGLE_AUTH_URL}
         state={{ type: 'google' }}
         onMouseEnter={onAuthLoad}
       >
-        <button className='googlebtn'>구글 로그인</button>
+        <button className='googlebtn'>
+          <img className='Login_logo' src={google} alt='구글 로그인 버튼' />
+        </button>
       </Link>
     </div>
   );
