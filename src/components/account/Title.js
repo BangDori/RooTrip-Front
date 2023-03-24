d90959cbcfed612232bd1c71e3acc032e6897df0
BangDori <qudwns8865@naver.com>
2023-03-25 00:58:50 +0900

    [Refactor] 회원가입 및 계정 찾기 Title 공통 component 병합

--- src/components/account/Title.js
+++ src/components/account/Title.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 import Logo from '@assets/Logo.png';
 import '@styles/register/register.scss';
 
-const Title = () => (
+const Title = ({ title }) => (
   <div className='Register_Title'>
     <Link to='/' className='Go_before_page'>
       <img className='logo' src={Logo} alt='로고사진' />
-      <span className='content'>JOIN MEMBERS</span>
+      <span className='content'>{title}</span>
     </Link>
   </div>
 );
