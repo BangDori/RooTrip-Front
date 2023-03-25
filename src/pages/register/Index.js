0192a1de35035e8611b831c3a40c6f469e343945
BangDori <qudwns8865@naver.com>
2023-03-25 17:23:58 +0900

    [Edit] RegisterTOC -> TOC 공통 컴포넌트로 통합

--- src/pages/register/Index.js
+++ src/pages/register/Index.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import RegisterTitle from './RegisterTitle';
-import RegisterTOC from './RegisterTOC';
 import RegisterContainer from './RegisterContainer';
+import Title from '@components/account/Title';
+import TOC from '@components/account/TOC';
+
+const table = [
+  {
+    id: 1,
+    name: '이름',
+  },
+  {
+    id: 2,
+    name: '이메일',
+    classname: 'email_check',
+  },
+  {
+    id: 3,
+    name: '닉네임',
+  },
+  {
+    id: 4,
+    name: '비밀번호',
+  },
+  {
+    id: 5,
+    name: '비밀번호 확인',
+  },
+];
 
 const Index = () => {
   return (
     <>
-      <RegisterTitle />
+      <Title title='JOIN MEMBERS' />
       <div className='Register_main'>
-        <RegisterTOC />
+        <TOC table={table} />
         <RegisterContainer />
       </div>
     </>
