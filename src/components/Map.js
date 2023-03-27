6a440e12d2fe216b1f90a1869347009d7f9570cd
BangDori <qudwns8865@naver.com>
2023-03-27 14:08:13 +0900

    [Edit] 사용하지 않는 hook 제거

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import ReactMapGL from 'react-map-gl';
 import CustomMarker from './map/CustomMarker';
 import CustomSource from './map/CustomSource';
