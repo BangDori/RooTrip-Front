83c3337ad3b9de1820b61921775a00fdf5dd7e20
BangDori <qudwns8865@naver.com>
2023-04-03 16:19:07 +0900

    [Setting] Map, AWS 환경변수 설정

---
+++ src/config/service.js
@@ -0,0 +1,10 @@
+// Map
+export const MAP_API_TOKEN = process.env.REACT_APP_MAP_API_TOKEN;
+export const MAP_API_STYLE = process.env.REACT_APP_MAP_API_STYLE;
+
+// AWS
+export const AWS_ACCESS_KEY = process.env.REACT_APP_AWS_ACCESS_KEY;
+export const AWS_SECRET_ACCESS_KEY =
+  process.env.REACT_APP_AWS_SECRET_ACCESS_KEY;
+export const AWS_REGION = process.env.REACT_APP_AWS_REGION;
+export const AWS_S3_BUCKET = process.env.REACT_APP_AWS_S3_BUCKET;
