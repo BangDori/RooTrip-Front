b35794155e488003e9e7f914839380c563697cc1
BangDori <qudwns8865@naver.com>
2023-04-06 13:47:14 +0900

    [Refactor] 지도 이벤트 기능 분리

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,23 +1,27 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import { useSelector } from 'react-redux';
-import loadable from '@loadable/component';
 import '@styles/components/Map.scss';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { useGetImages } from '@hooks/useGetImages';
 import { MAP_API_TOKEN, MAP_API_STYLE } from '@config/service';
-
-const CustomMarker = loadable(() => import('./map/CustomMarker'));
+import useMapEvents from '@hooks/useMapEvents';
+import CustomMarker from './map/CustomMarker';
 
 const Map = () => {
   const mapContainer = useRef();
   const map = useRef();
-  const hoveredStateId = useRef(null);
   const { accessToken } = useSelector((state) => state.accessToken);
-  const imageData = useGetImages(accessToken ? true : false);
 
+  // get image for custom marker
+  const imageData = useGetImages(!!accessToken);
+
+  // map event initialize
+  const { onMapLoad, onMapUnload } = useMapEvents({ map });
+
+  // map initialize
   useEffect(() => {
-    if (map.current) return; // initialize map only once
+    if (map.current) return; // 맵이 생성되어 있다면,
 
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
@@ -30,96 +34,28 @@ const Map = () => {
       antialias: false,
       doubleClickZoom: false,
       dragRotate: false,
-      // interactive: !accessToken ? false : true, //드래그 & 줌 둘다 막힘
+      interactive: !!accessToken, // accessToken ? Interactive : NULL
     });
 
-    if (accessToken) {
-      map.current.on('load', onLoad);
-    } else {
-      map.current.on('load', offLoad);
-    }
-
     return () => {
-      map.current.off('load', onLoad);
       map.current.remove();
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [accessToken]);
 
-  const onMapClick = useCallback(
-    (e) => {
-      const zoom = map.current.getZoom();
-      const { lngLat } = e;
+  // event loading
+  useEffect(() => {
+    if (map) {
+      accessToken
+        ? map.current.on('load', onMapLoad)
+        : map.current.on('load', onMapUnload);
 
-      map.current.flyTo({
-        essential: true,
-        center: lngLat,
-        zoom: zoom + 1,
-        speed: 1.25,
-        easing: (t) => t,
-      });
-    },
-    [map],
-  );
-
-  const onMapMove = useCallback((e) => {
-    map.current.getCanvas().style.cursor = 'pointer';
-
-    const { sourceLayer, id } = e.features[0];
-    console.log(sourceLayer);
-    map.current.setFeatureState(
-      {
-        source: 'composite',
-        sourceLayer: sourceLayer,
-        id: id,
-      },
-      { hover: true },
-    );
-
-    if (hoveredStateId.current !== id) {
-      map.current.setFeatureState(
-        {
-          source: 'composite',
-          sourceLayer: sourceLayer,
-          id: hoveredStateId.current,
-        },
-        { hover: false },
-      );
+      return () => {
+        accessToken
+          ? map.current.off('load', onMapLoad)
+          : map.current.off('load', onMapUnload);
+      };
     }
-
-    hoveredStateId.current = id;
-  }, []);
-
-  const onMapLeave = useCallback(() => {
-    map.current.getCanvas().style.cursor = 'initial';
-
-    if (hoveredStateId.current !== null) {
-      map.current.setFeatureState(
-        {
-          source: 'composite',
-          sourceLayer: 'Korea_Level1-72rlhs',
-          id: hoveredStateId.current,
-        },
-        { hover: false },
-      );
-    }
-  }, []);
-
-  const onLoad = useCallback(() => {
-    map.current.on('click', 'korea-fill', onMapClick);
-    map.current.on('mousemove', 'korea-fill', onMapMove);
-    map.current.on('mouseleave', 'korea-fill', onMapLeave);
-
-    return () => {
-      map.current.off('mousemove', 'korea-fill', onMapMove);
-      map.current.off('mouseleave', 'korea-fill', onMapLeave);
-      map.current.off('click', 'korea-fill', onMapClick);
-    };
-  }, [onMapClick, onMapMove, onMapLeave]);
-
-  const offLoad = useCallback(() => {
-    map.current.setPaintProperty('korea-fill', 'fill-opacity', 1);
-  }, []);
+  }, [map, accessToken, onMapLoad, onMapUnload]);
 
   return (
     <div className='map-container'>
