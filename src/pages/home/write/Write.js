834744032f73c4ff96f5181b4ec1144490311d6a
BangDori <qudwns8865@naver.com>
2023-04-19 01:08:40 +0900

    [Feat] preSignedUrl에 사진 업로드

--- src/pages/home/write/Write.js
+++ src/pages/home/write/Write.js
@@ -1,5 +1,6 @@
 import React, { useCallback, useState } from 'react';
 import '@styles/home/Write.scss';
+import { getPreSignedUrl, uploadFileToS3 } from '@services/image';
 import UploadImages from './UploadImages';
 import SelectImages from './SelectImages';
 import WriteContent from './WriteContent';
@@ -9,18 +10,42 @@ const Write = ({ onChangeMode }) => {
   const [photos, setPhotos] = useState([]);
   const [routes, setRoutes] = useState([]);
   const [article, setArticle] = useState({});
+  const [imageUrls, setImageUrls] = useState([]);
 
   const onMovePage = useCallback(
-    (move) => {
+    async (move) => {
       if (currentPage + move === -1) {
         onChangeMode();
         return;
       }
 
       if (currentPage + move === 3) {
-        // photos aws 서버에 올리기
+        // url 받아오기
+        const fileNames = photos.map((photo) => photo.fileName);
+        const url = await getPreSignedUrl(fileNames);
 
-        // 서버로 데이터 묶어서 전송하기
+        // pre-signed url에 사진 파일 전송하기
+        const newPhotos = await Promise.all(
+          photos.map(async (photo) => {
+            const data = await fetch(photo.url);
+            const blob = await data.blob();
+            const file = new File([blob], `${Date.now()}-image.jpg`, {
+              type: 'image/jpeg',
+            });
+
+            try {
+              const signedUrl = await uploadFileToS3(file, url);
+
+              return photo.url !== signedUrl ? signedUrl : photo.url;
+            } catch (e) {
+              return null;
+            }
+          }),
+        );
+
+        // s3에 업로드된 주소로 url 변경
+
+        // 서버로 데이터 전송하기
         const writing = {
           photos,
           routes,
--- src/services/image.js
+++ src/services/image.js
@@ -7,16 +7,58 @@ import axios from 'axios';
  * @param {*} longitude 경도
  * @returns 행정구역
  */
-async function getAddress(latitude, longitude) {
-  const point = await axios
-    .get(
-      `${MAIN_SERVER}/api/photo/reverse?latitude=${latitude}&longitude=${longitude}`,
-    )
-    .then((res) => {
-      return `${res.data.city} ${res.data.first}`;
-    });
+export async function getAddress(latitude, longitude) {
+  try {
+    const point = await axios
+      .get(
+        `${MAIN_SERVER}/api/photo/reverse?latitude=${latitude}&longitude=${longitude}`,
+      )
+      .then((res) => {
+        return `${res.data.city} ${res.data.first}`;
+      });
 
-  return point;
+    return point;
+  } catch (e) {
+    return e;
+  }
 }
 
-export default getAddress;
+/**
+ * AWS S3 pre-signed url을 받아오기 위한 함수
+ * @returns pre-signed url
+ */
+export async function getPreSignedUrl(fileNames) {
+  try {
+    const url = await axios
+      .post(`${MAIN_SERVER}/api/photo/signed`, fileNames)
+      .then((res) => res.data);
+
+    return url;
+  } catch (e) {
+    return e;
+  }
+}
+
+/**
+ * Pre-signed Url에 파일 업로드하기
+ * @param {*} file 파일
+ * @param {*} preSignedUrl Pre-signed url
+ * @returns 파일이 업로드된 주소
+ */
+export async function uploadFileToS3(file, preSignedUrl) {
+  try {
+    const res = await fetch(
+      new Request(preSignedUrl, {
+        method: 'PUT',
+        body: file,
+        headers: new Headers({
+          'Content-Type': 'image/jgeg',
+        }),
+      }),
+    );
+
+    return res;
+  } catch (e) {
+    return e;
+  }
+}
