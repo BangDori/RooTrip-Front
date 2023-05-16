e67c992debe7dd31ed12837befeca6eb8a517476
BangDori <qudwns8865@naver.com>
2023-05-16 16:38:23 +0900

    [Style] TOC 컴포넌트 스타일 방식 수정

--- src/components/wrapper/TOC.js
+++ src/components/wrapper/TOC.js
@@ -1,35 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const StyledTOC = styled.div`
-  float: left;
-  width: 38%;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  border-right: 2px solid black;
-  padding-right: 30px;
-  padding-top: 15px;
-
-  span {
-    font-size: 20px;
-    margin-bottom: 46px;
-  }
-
-  .email_check {
-    margin-bottom: 122px;
-  }
-`;
+import '@styles/components/TOC.scss';
 
 const TOC = ({ table }) => {
   return (
-    <StyledTOC>
+    <div className='register-table'>
       {table.map((content) => (
         <span key={content.id} className={content.classname}>
           {content.name}
         </span>
       ))}
-    </StyledTOC>
+    </div>
   );
 };
 
