10df48fea96e266113d2b4f5bd58ddcbed455886
BangDori <qudwns8865@naver.com>
2023-05-31 20:11:11 +0900

    [Feat] 마커 hover, active 효과 추가

--- src/components/map/CustomMarker.js
+++ src/components/map/CustomMarker.js
@@ -1,9 +1,9 @@
 import React, { useCallback, useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { load } from '@store/article';
 
-const InvertedTriangle = () => (
+const InvertedTriangle = ({ isClick }) => (
   <svg
     width='32'
     height='40'
@@ -12,6 +12,7 @@ const InvertedTriangle = () => (
     xmlns='http://www.w3.org/2000/svg'
   >
     <path
+      className={isClick ? 'clicked' : ''}
       d='M4.89372 194.124C2.61773 195.521 -0.235684 193.574 0.331282 191.012L41.9368 2.95425C42.397 0.873958 44.876 -0.0896543 46.6514 1.12161L172.064 86.6836C173.84 87.8949 173.76 90.4951 171.912 91.6289L4.89372 194.124Z'
       fill='white'
       transform='rotate(-36 87 87)'
@@ -22,6 +23,8 @@ const InvertedTriangle = () => (
 const CustomMarker = ({ map, id, src, coordinate, accessToken }) => {
   const markerRef = useRef(null);
   const dispatch = useDispatch();
+  const { id: clickedId } = useSelector((state) => state.article);
+  const isClick = id === clickedId;
 
   /**
    * Marker click event
@@ -53,9 +56,10 @@ const CustomMarker = ({ map, id, src, coordinate, accessToken }) => {
 
   return (
     <div ref={markerRef}>
-      <InvertedTriangle />
+      <InvertedTriangle isClick={isClick} />
       <div className='map-marker-image'>
         <img
+          className={isClick ? 'clicked' : ''}
           src={src}
           alt='marker'
           style={{ cursor: accessToken ? 'pointer' : 'initial' }}
