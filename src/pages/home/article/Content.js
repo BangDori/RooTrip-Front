9db9caf357869af83c81be32d102fe578a3cbb3e
BangDori <qudwns8865@naver.com>
2023-06-04 16:12:33 +0900

    [Feat] 이미지 슬라이드바 기능

--- src/pages/home/article/Content.js
+++ src/pages/home/article/Content.js
@@ -1,10 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { getComments } from '@services/post';
+
 import Profile from '@assets/DefaultProfileImage.png';
+import Like from '@assets/Like.png';
+import NotLike from '@assets/NotLike.png';
+import Photos from './Photos';
 
 const Content = ({ accessToken, postId, post, onClose }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [comments, setComments] = useState([]);
+  const [currentPhoto, setCurrentPhoto] = useState(0);
+
   const { user, photos, title, content } = post;
   const { profileImage, name } = user;
 
@@ -24,20 +30,32 @@ const Content = ({ accessToken, postId, post, onClose }) => {
     loadComments();
   }, [accessToken, postId]);
 
+  const onChangePhoto = useCallback((move) => {
+    setCurrentPhoto((prevPhoto) => prevPhoto + move);
+  }, []);
+
   if (isLoading) return null;
+
+  // eslint-disable-next-line no-console
+  console.log(comments);
 
   return (
     <div className='modal-post'>
       <div className='modal-left'>
         <div className='modal-photos'>
-          {photos.map((photo) => (
+          <Photos
+            photos={photos}
+            current={currentPhoto}
+            onChangePhoto={onChangePhoto}
+          />
+          {/* {photos.map((photo) => (
             <div key={photo.id} className='modal-photo'>
               <img
                 src={photo.imageUrl}
                 alt={photo.city + photo.first + photo.second}
               />
             </div>
-          ))}
+          ))} */}
         </div>
       </div>
       <div className='modal-right'>
@@ -52,8 +70,28 @@ const Content = ({ accessToken, postId, post, onClose }) => {
             <p className='modal-content'>{content}</p>
           </div>
           <div className='modal-comments'>
+            <div className='modal-comments-tot-count-box'>
+              좋아요 {comments.length}개
+            </div>
             {comments.map((comment) => (
-              <div key={comment.id}>{comment.comment}</div>
+              <div key={comment.id} className='modal-comment'>
+                <div className='modal-comment-user'>
+                  <img src={profileImage || Profile} alt='profile image' />
+                  <h5 className='profile_name'>{name}</h5>
+                  <span className='modal-comment-createAt'>
+                    {comment.createdAt}
+                  </span>
+                </div>
+                <div className='modal-comment-box'>
+                  <p className='modal-comment-content'>{comment.comment}</p>
+                </div>
+                <div className='modal-comment-like'>
+                  <img src={NotLike} alt='like button' />
+                  <span className='modal-comment-like-count'>
+                    {comment.like}
+                  </span>
+                </div>
+              </div>
             ))}
           </div>
         </div>
