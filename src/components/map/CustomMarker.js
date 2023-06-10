478a456b5c99cfaac2658a08d223eb931a9a43c7
BangDori <qudwns8865@naver.com>
2023-06-11 05:19:24 +0900

    [!BREAKING_CHANGE] 지도 라이브러리 변경

--- src/components/map/CustomMarker.js
+++ src/components/map/CustomMarker.js
@@ -1,84 +1,40 @@
-import React, { useCallback, useEffect, useRef } from 'react';
-import mapboxgl from 'mapbox-gl';
+import { useCallback, useEffect, useState } from 'react';
+import { Marker } from 'react-map-gl';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { removeOnStore } from '@store/marker';
-import { load, exit } from '@store/article';
-import Menu from '@constants/menu';
+import { load } from '@store/article';
 
-const InvertedTriangle = ({ isClick }) => (
-  <svg
-    width='32'
-    height='40'
-    viewBox='0 0 174 195'
-    fill='none'
-    xmlns='http://www.w3.org/2000/svg'
-  >
-    <path
-      className={isClick ? 'clicked' : ''}
-      d='M4.89372 194.124C2.61773 195.521 -0.235684 193.574 0.331282 191.012L41.9368 2.95425C42.397 0.873958 44.876 -0.0896543 46.6514 1.12161L172.064 86.6836C173.84 87.8949 173.76 90.4951 171.912 91.6289L4.89372 194.124Z'
-      fill='white'
-      transform='rotate(-36 87 87)'
-    />
-  </svg>
-);
+const CustomMarker = ({ postId, lng, lat, imageURl }) => {
+  const [isClick, setIsClick] = useState(false);
 
-const CustomMarker = ({ map, id, src, coordinate, accessToken }) => {
-  const markerRef = useRef(null);
+  const { accessToken } = useSelector((state) => state.accessToken);
+  const { postId: id } = useSelector((state) => state.article);
   const dispatch = useDispatch();
-  const { id: clickedId } = useSelector((state) => state.article);
-  const { removeID } = useSelector((state) => state.marker);
-  const clickedMenu = useSelector((state) => state.menu);
-  const isClick = id === clickedId;
 
   useEffect(() => {
-    if (id === removeID) {
-      // markerRef.current = null;
-      dispatch(exit());
-      dispatch(removeOnStore({ id }));
-    }
-  }, [dispatch, removeID, id]);
+    if (id === postId) setIsClick(true);
+    else setIsClick(false);
+  }, [postId, id]);
 
-  /**
-   * Marker click event
-   * Marker active & load Article
-   */
-  const onMarkerClick = useCallback(
-    () => dispatch(load({ id })),
-    [id, dispatch],
-  );
-
-  useEffect(
-    // eslint-disable-next-line consistent-return
-    () => {
-      if (markerRef.current && clickedMenu === Menu.TRIP) {
-        const marker = new mapboxgl.Marker().setLngLat(coordinate).addTo(map);
-
-        // 마커 클릭 핸들러 등록
-        if (accessToken)
-          marker.getElement().addEventListener('click', onMarkerClick);
-
-        // 컴포넌트 언마운트 시 마커 제거
-        return () => {
-          if (marker) marker.remove();
-        };
-      }
-    },
-    [accessToken, map, coordinate, clickedMenu, onMarkerClick],
-  );
+  const onMarkerClick = useCallback(() => {
+    dispatch(load({ postId }));
+  }, [dispatch, postId]);
 
   return (
-    <div ref={markerRef}>
-      <InvertedTriangle isClick={isClick} />
-      <div className='map-marker-image'>
+    <Marker
+      longitude={lng}
+      latitude={lat}
+      anchor='bottom'
+      onClick={onMarkerClick}
+    >
+      <div className={`map-marker-image ${isClick ? 'clicked' : ''}`}>
         <img
-          className={isClick ? 'clicked' : ''}
-          src={src}
+          src={imageURl}
           alt='marker'
           style={{ cursor: accessToken ? 'pointer' : 'initial' }}
         />
       </div>
-    </div>
+    </Marker>
   );
 };
 
--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,65 +1,37 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import MapGL from 'react-map-gl';
+import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { updateLocation } from '@store/location';
 import { MAP_API_TOKEN, MAP_API_STYLE } from '@config/service';
 import { removeAll } from '@store/marker';
+import { updateLocation } from '@store/location';
 import CustomMarker from './CustomMarker';
 import '@styles/components/Map.scss';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+const viewport = {
+  longitude: 131.1,
+  latitude: 36.4395,
+  zoom: 5.5, // Default
+};
+const zoom = {
+  minZoom: 5.5,
+  maxZoom: 10,
+};
+
 const Map = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const mapContainer = useRef();
-  const map = useRef(null);
   const { accessToken } = useSelector((state) => state.accessToken);
-  const { markers } = useSelector((state) => state.marker);
   const { isSetLocation } = useSelector((state) => state.location);
+  const marker = useSelector((state) => state.marker);
   const dispatch = useDispatch();
 
-  // eslint-disable-next-line consistent-return
-  useEffect(() => {
-    if (isSetLocation && map.current) {
-      const handleClick = (e) => {
-        const { lat, lng } = e.lngLat;
-        dispatch(updateLocation({ lat, lng }));
-      };
-
-      map.current.on('click', handleClick);
-
-      return () => {
-        map.current.off('click', handleClick);
-      };
-    }
-  }, [dispatch, isSetLocation]);
-
-  // map initialize
-  useEffect(() => {
-    setIsLoading(true);
-
-    if (!map.current) {
-      map.current = new mapboxgl.Map({
-        container: mapContainer.current,
-        accessToken: MAP_API_TOKEN,
-        style: MAP_API_STYLE,
-        center: [131.1, 36.4395],
-        zoom: 5.5,
-        minZoom: 5.5,
-        maxZoom: 10,
-        antialias: false,
-        doubleClickZoom: false,
-        dragRotate: false,
-        interactive: !!accessToken, // accessToken ? Interactive : NULL
-        maxBounds: [
-          [112.704171, 32.745446],
-          [145.046554, 41.038954],
-        ],
-      });
-    }
-
-    map.current.on('load', () => setIsLoading(false));
-  }, [accessToken]);
+  const setLocationHandler = useCallback(
+    (e) => {
+      const { lat, lng } = e.lngLat;
+      dispatch(updateLocation({ lat, lng }));
+    },
+    [dispatch],
+  );
 
   const removeAllMarkerHandler = useCallback(() => {
     if (window.confirm('모두 삭제?')) {
@@ -67,25 +39,28 @@ const Map = () => {
     }
   }, [dispatch]);
 
-  let markerComponent = null;
+  // eslint-disable-next-line no-console
+  console.log(marker);
 
-  if (markers && map.current) {
-    markerComponent = markers.map((marker) => {
-      if (!marker.coordinate) return null;
+  let markers = null;
 
-      const coordinateString = marker.coordinate
+  if (marker && accessToken) {
+    markers = marker.map((mark) => {
+      if (!mark.coordinate) return null;
+
+      const coordinateString = mark.coordinate
         .replace('POINT(', '')
         .replace(')', '');
-      const [lng, lat] = coordinateString.split(' ');
+
+      const [lat, lng] = coordinateString.split(' ');
 
       return (
         <CustomMarker
-          key={marker.id}
-          id={marker.postId}
-          map={map.current}
-          src={marker.imageUrl}
-          coordinate={[lat, lng]}
-          accessToken={accessToken}
+          key={mark.id}
+          postId={mark.postId}
+          lat={lat}
+          lng={lng}
+          imageURl={mark.imageUrl}
         />
       );
     });
@@ -93,7 +68,18 @@ const Map = () => {
 
   return (
     <div className='map-container'>
-      <div className='ocean-container' />
+      <MapGL
+        initialViewState={viewport}
+        style={{ width: '100vw', height: '100vh' }}
+        mapStyle={MAP_API_STYLE}
+        mapboxAccessToken={MAP_API_TOKEN}
+        onClick={isSetLocation && setLocationHandler}
+        scrollZoom={accessToken} // accessToken ? true : false
+        dragPan={accessToken} // accessToken ? true : false
+        {...zoom}
+      >
+        {markers}
+      </MapGL>
       <button
         style={{
           position: 'absolute',
@@ -106,9 +92,7 @@ const Map = () => {
       >
         전체 삭제
       </button>
-      <div ref={mapContainer} className='map-container'>
-        {markerComponent}
-      </div>
+      <div className='ocean-container'></div>
     </div>
   );
 };
