93a37fa89276885493c80cdf0f7f6fb5f2d9a61e
BangDori <qudwns8865@naver.com>
2023-06-13 03:22:21 +0900

    [Refactor] 조건문 제거 및 반환값 통일

--- src/components/map/CustomMarker.js
+++ src/components/map/CustomMarker.js
@@ -3,11 +3,13 @@ import { Marker } from 'react-map-gl';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { load } from '@store/article';
+import Menu from '@constants/menu';
 
 const CustomMarker = ({ postId, lng, lat, imageURl, order }) => {
   const [isClick, setIsClick] = useState(false);
 
   const { accessToken } = useSelector((state) => state.accessToken);
+  const menu = useSelector((state) => state.marker.menu);
   const { postId: id } = useSelector((state) => state.article);
   const dispatch = useDispatch();
 
@@ -20,21 +22,6 @@ const CustomMarker = ({ postId, lng, lat, imageURl, order }) => {
     dispatch(load({ postId }));
   }, [dispatch, postId]);
 
-  if (order) {
-    return (
-      <Marker longitude={lng} latitude={lat} anchor='bottom'>
-        <div className='map-marker-image order-marker'>
-          <img
-            src={imageURl}
-            alt='marker'
-            style={{ cursor: accessToken ? 'pointer' : 'initial' }}
-          />
-          <span>{order}</span>
-        </div>
-      </Marker>
-    );
-  }
-
   return (
     <Marker
       longitude={lng}
@@ -42,12 +29,17 @@ const CustomMarker = ({ postId, lng, lat, imageURl, order }) => {
       anchor='bottom'
       onClick={onMarkerClick}
     >
-      <div className={`map-marker-image ${isClick ? 'clicked' : ''}`}>
+      <div
+        className={`map-marker-image ${
+          isClick && menu === Menu.TRIP ? 'clicked' : ''
+        } ${menu !== Menu.TRIP ? 'no-effect-marker' : ''}`}
+      >
         <img
           src={imageURl}
           alt='marker'
-          style={{ cursor: accessToken ? 'pointer' : 'initial' }}
+          style={{ cursor: menu === Menu.TRIP ? 'pointer' : 'initial' }}
         />
+        {order && <span>{order}</span>}
       </div>
     </Marker>
   );
