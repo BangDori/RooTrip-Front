8fd6971fa76b943bb988a77bdef0ae25e35f8924
BangDori <qudwns8865@naver.com>
2023-06-13 20:53:39 +0900

    [Design] 마이페이지 메뉴 아이콘 통합

--- src/pages/home/HomeMenu.js
+++ src/pages/home/HomeMenu.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import Menu from '@constants/menu';
+import MyPageIcon from '@assets/menu/mypage.png';
 import FriendIcon from '@assets/menu/friend.png';
 import TripIcon from '@assets/menu/trip.png';
 import RouteIcon from '@assets/menu/route.png';
 import LogIcon from '@assets/menu/log.png';
 import WriteIcon from '@assets/menu/write.png';
 import '@styles/home/Nav.scss';
-import { useSelector } from 'react-redux';
 
 const menuItems = [
   { id: Menu.FRIEND, label: '친구 게시글', icon: FriendIcon },
@@ -34,6 +36,18 @@ const HomeGnb = ({ onClickMenu }) => {
     <nav className='side_nav'>
       <div className='menu'>
         <div className='menu_Icon'>
+          <Link to='/mypage' style={{ textDecoration: 'none' }}>
+            <div className='icon_up'>
+              <button type='button'>
+                <img
+                  src={MyPageIcon}
+                  alt='마이페이지'
+                  style={{ width: '24px' }}
+                />
+              </button>
+              <span>마이페이지</span>
+            </div>
+          </Link>
           {menuItems.map((menuItem) => (
             <div
               key={menuItem.id}
