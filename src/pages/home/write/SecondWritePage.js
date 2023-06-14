efd84e1e3a273ac2be929af08bdb1c412b7167ad
BangDori <qudwns8865@naver.com>
2023-06-14 17:13:58 +0900

    [Feat] 경로 최소 하나 이상 선택

--- src/pages/home/write/SecondWritePage.js
+++ src/pages/home/write/SecondWritePage.js
@@ -5,6 +5,7 @@ import { useDispatch } from 'react-redux';
 import { resetMap, setChangeCoordinate } from '@store/map';
 import { insert, remove, removeAll } from '@store/marker';
 import { changeCityToCoordinate } from '@utils/metadata';
+import Modal from '@components/wrapper/Modal';
 
 function routeReducer(routes, action) {
   switch (action.type) {
@@ -26,7 +27,16 @@ const SecondWritePage = ({
 }) => {
   const [routes, dispatchRoute] = useReducer(routeReducer, prevRoutes);
   const [routesOnMap, setRoutesOnMap] = useState([]);
+  const [showMessage, setShowMessage] = useState('');
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (showMessage) {
+      setTimeout(() => {
+        setShowMessage('');
+      }, 2000);
+    }
+  }, [showMessage]);
 
   useEffect(() => {
     if (routesOnMap.length === 0) {
@@ -77,44 +87,58 @@ const SecondWritePage = ({
   }, [dispatch, onMovePage]);
 
   const onNextPageHandler = useCallback(() => {
+    if (routes.length === 0) {
+      setShowMessage('경로를 하나 이상 설정해주세요.');
+      return;
+    }
+
     onMovePage(1);
     setRoutes(routes);
   }, [routes, onMovePage, setRoutes]);
 
   return (
-    <div className='Second_modal'>
-      <div className='Modal_head'>
-        <button
-          type='button'
-          className='MoveModal F'
-          onClick={onPrevPageHandler}
-        >
-          이전
-        </button>
-        <span>게시 순서 및 경로 설정</span>
-        <button
-          type='button'
-          className='MoveModal Co'
-          onClick={onNextPageHandler}
-        >
-          다음
-        </button>
-      </div>
-      <div className='Write_content'>
-        <div className='Write_list'>
-          {photos.map((photo, idx) => (
-            <Photo
-              key={idx}
-              id={idx + 1}
-              photo={photo}
-              addRoute={addRoute}
-              clicked={routes.indexOf(idx + 1) + 1}
-              updateCoordinate={updateCoordinate}
-            />
-          ))}
+    <>
+      <div className='Second_modal'>
+        <div className='Modal_head'>
+          <button
+            type='button'
+            className='MoveModal F'
+            onClick={onPrevPageHandler}
+          >
+            이전
+          </button>
+          <span>게시 순서 및 경로 설정</span>
+          <button
+            type='button'
+            className='MoveModal Co'
+            onClick={onNextPageHandler}
+          >
+            다음
+          </button>
+        </div>
+        <div className='Write_content'>
+          <div className='Write_list'>
+            {photos.map((photo, idx) => (
+              <Photo
+                key={idx}
+                id={idx + 1}
+                photo={photo}
+                addRoute={addRoute}
+                clicked={routes.indexOf(idx + 1) + 1}
+                updateCoordinate={updateCoordinate}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+      {showMessage && (
+        <Modal background='white'>
+          <div className='modal-message'>
+            <p>{showMessage}</p>
+          </div>
+        </Modal>
+      )}
+    </>
   );
 };
 
