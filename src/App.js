f8e4468f2c6ad3461982202b82886d54207ea207
BangDori <qudwns8865@naver.com>
2023-09-03 21:11:49 +0900

    [Feat] React-Router v6.15 적용

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import RootLayout from './pages/RootLayout';
-import ErrorPage from './pages/Error';
-import LoginPage from './pages/Login';
+import RootLayout from '@pages/RootLayout';
+import ErrorPage from '@pages/ErrorPage';
+import LoginPage, { action as loginAction } from '@pages/LoginPage';
+import TripPage from '@pages/TripPage';
 
-import AuthLayout from './pages/AuthLayout';
+import AuthLayout from '@pages/AuthLayout';
+
+import { loader as socialLoginLoader } from '@pages/SocialLogin';
+
+import { loader as logoutLoader } from '@pages/Logout';
+import {
+  tokenLoader,
+  restrictAccessWithNoToken,
+  restrictAccessWithToken,
+} from '@utils/token';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    id: 'root',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    loader: tokenLoader,
     children: [
       {
         index: true,
         element: <LoginPage />,
+        loader: restrictAccessWithToken,
+        action: loginAction,
+      },
+      {
+        path: 'trip',
+        element: <TripPage />,
+        loader: restrictAccessWithNoToken,
+        children: [
+          {
+            path: ':postId',
+          },
+        ],
+      },
+      {
+        path: 'profile',
+        loader: restrictAccessWithNoToken,
+        children: [
+          {
+            path: ':userId',
+          },
+          {
+            path: 'edit',
+          },
+        ],
+      },
+      {
+        path: 'route',
+        loader: restrictAccessWithNoToken,
+      },
+      {
+        path: 'write',
+        loader: restrictAccessWithNoToken,
+        children: [
+          {
+            path: '1',
+          },
+          {
+            path: '2',
+          },
+          {
+            path: '3',
+          },
+        ],
       },
     ],
   },
   {
     path: '/auth',
     element: <AuthLayout />,
+    children: [
+      {
+        path: 'signup',
+        loader: restrictAccessWithToken,
+      },
+      {
+        path: 'account',
+        loader: restrictAccessWithToken,
+      },
+    ],
+  },
+  {
+    path: 'oauth/:provider/*',
+    loader: socialLoginLoader,
+  },
+  {
+    path: '/logout',
+    loader: logoutLoader,
   },
 ]);
 
