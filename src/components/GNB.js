7d4f2705f06b4798a07966cb9c80d368c732d3f9
BangDori <qudwns8865@naver.com>
2023-09-07 15:46:14 +0900

    [Feat] GNB 추가

---
+++ src/components/GNB.js
@@ -0,0 +1,66 @@
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faUser,
+  faPlaneUp,
+  faMapLocationDot,
+  faCamera,
+} from '@fortawesome/free-solid-svg-icons';
+
+import logoIcon from '@assets/logo-icon.png';
+import '@styles/GNB.scss';
+
+const GNB = () => {
+  return (
+    <div className='gnb'>
+      <NavLink to='/trip'>
+        <img src={logoIcon} className='logo-icon' alt='logo image' />
+      </NavLink>
+
+      <ul className='menu'>
+        <li>
+          <NavLink
+            to='/profile'
+            className={({ isActive }) => (isActive ? 'isActive' : undefined)}
+          >
+            <div className='menu-icon'>
+              <FontAwesomeIcon icon={faUser} />
+            </div>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/trip'
+            className={({ isActive }) => (isActive ? 'isActive' : undefined)}
+          >
+            <div className='menu-icon'>
+              <FontAwesomeIcon icon={faPlaneUp} />
+            </div>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/route'
+            className={({ isActive }) => (isActive ? 'isActive' : undefined)}
+          >
+            <div className='menu-icon'>
+              <FontAwesomeIcon icon={faMapLocationDot} />
+            </div>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/write'
+            className={({ isActive }) => (isActive ? 'isActive' : undefined)}
+          >
+            <div className='menu-icon'>
+              <FontAwesomeIcon icon={faCamera} />
+            </div>
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default GNB;
