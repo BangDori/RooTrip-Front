a57070b2239a7a668f1b0ffb76e6cf2114517c86
BangDori <qudwns8865@naver.com>
2023-09-12 20:20:54 +0900

    [Feat] isSubmitting 으로 진행 상태 관리

--- src/components/root/write/Completion.js
+++ src/components/root/write/Completion.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
@@ -7,31 +6,23 @@ import completionGIF from '@assets/completion.gif';
 import '@styles/root/write/Completion.scss';
 
 const Completion = ({ isSubmitting }) => {
-  const [isSending, setIsSending] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsSending(false);
-    }, isSubmitting);
-  }, [isSubmitting]);
-
   return (
     <div className='post-wrapper'>
       <div className='completion-box'>
-        {isSending && (
+        {isSubmitting && (
           <img
             className='gif-animation'
             src={completionGIF}
             alt='completion gif'
           />
         )}
-        {!isSending && (
+        {!isSubmitting && (
           <FontAwesomeIcon icon={faCircleCheck} width={160} height={160} />
         )}
       </div>
       <p className='completion-message'>
-        {isSending && '여행을 공유하고 있습니다...'}
-        {!isSending && '여행이 정상적으로 공유되었습니다'}
+        {isSubmitting && '여행을 공유하고 있습니다...'}
+        {!isSubmitting && '여행이 정상적으로 공유되었습니다'}
       </p>
       <Link to='/trip'>
         <button className='confirm-button'>확인</button>
