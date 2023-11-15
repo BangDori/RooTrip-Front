2bc1ed94b39f3b326ee6d62383c51827a15069af
BangDori <qudwns8865@naver.com>
2023-11-16 00:23:59 +0900

    Feat: 지도 관련 상태 수정

--- src/store/map.js
+++ src/store/map.js
@@ -3,32 +3,18 @@ import { createSlice } from '@reduxjs/toolkit';
 const mapSlice = createSlice({
   name: 'map',
   initialState: {
-    viewType: 'region',
     center: [131.1, 36.4395],
-    markerCount: 10,
-    polygon: '',
     zoom: 5.5,
   },
   reducers: {
     changeMap: (state, action) => {
-      const { viewType, center, polygon, zoom } = action.payload;
+      const { center, zoom } = action.payload;
 
-      let markerCount = 10;
-      if (viewType === 'city') {
-        markerCount = 15;
-      }
-
-      state.viewType = viewType;
       state.center = center;
-      state.markerCount = markerCount;
-      state.polygon = polygon;
       state.zoom = zoom;
     },
     resetMap: (state) => {
-      state.viewType = 'region';
       state.center = [131.1, 36.4395];
-      state.markerCount = 8;
-      state.polygon = '';
       state.zoom = 5.5;
     },
   },
